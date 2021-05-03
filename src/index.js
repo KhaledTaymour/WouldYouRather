1b6cd46afbe462a3601260fb541e1410eccbffd6
KhaledTaymour <khaled.taymour@gmail.com>
2021-05-03 21:33:05 +0200

    [feat]: create store, reducers, middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "container/app/App.js";
 
+// store
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "redux/reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/redux/actionTypes.js
@@ -0,0 +1,8 @@
+// authedUser
+export const SET_AUTHED_USER = 'SET_AUTHED_USER';
+
+// users
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+
+// questions
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
